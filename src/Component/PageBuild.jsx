363c65648221d9fa077cd0d7b466dd0013f43572
Demetrio Pacheco <demetriodeis@gmail.com>
2024-10-20 11:14:59 -0300

    change PageBuild

--- src/Component/PageBuild.jsx
+++ src/Component/PageBuild.jsx
@@ -3,7 +3,7 @@ import styles from './PageBuild.module.css'
 export function PageBuild() {
     return (
         <div className={styles.pageBuild}>
-            <h1><Barricade size={32} color="#f4d50b" weight="fill" />Página em Construção<Barricade size={32} color="#f4d50b" weight="fill"/></h1>
+            <h1><Barricade size={28} color="#f4d50b" weight="fill" />Página em Construção</h1>
             <p>Estamos trabalhando duro para trazer novidades. Volte em breve!</p>
             <div className={styles.loader}></div>
         </div>
